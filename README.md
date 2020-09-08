ec0c3ee2c199bd08bfeb86c4a0fdd38e23f373ac
overlookmotel <theoverlookmotel@gmail.com>
2020-09-08 12:29:19 +0100

    Initial release

--- README.md
+++ README.md
@@ -9,7 +9,84 @@
 
 ## Usage
 
-This module is under development and not ready for use yet.
+### Standard values
+
+```js
+const typeOf = require('native-type-of');
+
+// Primitives
+typeOf('abc') === 'string';
+typeOf(123) === 'number';
+typeOf(true) === 'boolean';
+typeOf(Symbol()) === 'symbol';
+typeOf(100n) === 'bigint';
+typeOf(null) === 'null';
+typeOf(undefined) === 'undefined';
+
+// Functions
+typeOf(function() {}) === 'Function';
+typeOf(() => {}) === 'Function';
+typeOf(async function() {}) === 'Function';
+typeOf(function*() {}) === 'Function';
+typeOf(async function*() {}) === 'Function';
+
+// Objects
+typeOf({}) === 'Object';
+class C {}
+typeOf(new C()) === 'Object';
+
+// Built-ins
+typeOf([]) === 'Array';
+typeOf(/^abc$/) === 'RegExp';
+typeOf(new Date()) === 'Date';
+typeOf(Promise.resolve()) === 'Promise';
+typeOf(new Set()) === 'Set';
+typeOf(new Map()) === 'Map';
+typeOf(new WeakSet()) === 'WeakSet';
+typeOf(new WeakMap()) === 'WeakMap';
+typeOf(new WeakRef({})) === 'WeakRef';
+typeOf(new FinalizationRegistry(() => {})) === 'FinalizationRegistry';
+
+// Boxed primitives
+typeOf(new String('')) === 'String';
+typeOf(new Number(123)) === 'Number';
+typeOf(new Boolean(true)) === 'Boolean';
+
+// Errors
+typeOf(new Error()) === 'Error';
+typeOf(new TypeError()) === 'TypeError';
+typeOf(new ReferenceError()) === 'ReferenceError';
+// ...etc
+
+// Buffers
+typeOf(new ArrayBuffer(8)) === 'ArrayBuffer';
+typeOf(new SharedArrayBuffer(8)) === 'SharedArrayBuffer';
+typeOf(new Uint8Array([1, 2, 3])) === 'Uint8Array';
+typeOf(new Int32Array([1, 2, 3])) === 'Int32Array';
+// ...etc
+```
+
+### Values with altered prototype
+
+If the prototype chain is altered, this package aims to return the *native* type of the underlying object.
+
+This is where this package differs from others like [kind-of](https://www.npmjs.com/package/kind-of).
+
+```js
+const map = new Map();
+Object.setPrototypeOf(map, Array.prototype);
+typeOf(map) === 'Map';
+```
+
+### Known issues
+
+There are a few rare cases where native type cannot be correctly determined:
+
+* `typeOf( Object.setPrototypeOf( Promise.resolve(), Array.prototype ) ) === 'Object'` ([issue](https://github.com/overlookmotel/native-type-of/issues/1))
+* `typeOf( Object.setPrototypeOf( new Error(), Map.prototype ) ) === 'Object'` ([issue](https://github.com/overlookmotel/native-type-of/issues/2))
+* `typeOf( Object.setPrototypeOf( {}, TypeError.prototype ) ) === 'TypeError'` ([issue](https://github.com/overlookmotel/native-type-of/issues/3))
+
+These are the only known cases where the result is inaccurate.
 
 ## Versioning
 
--- test/index.test.js
+++ test/index.test.js
@@ -8,13 +8,120 @@
 // Modules
 const typeOf = require('native-type-of');
 
-// Init
-require('./support/index.js');
-
 // Tests
 
-describe('tests', () => {
-	it.skip('all', () => { // eslint-disable-line jest/no-disabled-tests
-		expect(typeOf).not.toBeUndefined();
+const hasOwnProp = Object.prototype.hasOwnProperty,
+	functionToString = Function.prototype.toString,
+	// eslint-disable-next-line node/no-unsupported-features/es-builtins
+	bigIntSupported = typeof BigInt !== 'undefined';
+
+const globals = [];
+for (const name of Object.getOwnPropertyNames(global)) {
+	if (!/^[A-Z]/.test(name)) continue;
+	const ctor = global[name];
+	if (typeof ctor !== 'function') continue;
+	if (!hasOwnProp.call(ctor, 'prototype')) continue;
+	if (!functionToString.call(ctor).includes('[native code]')) continue;
+	globals.push([name, ctor]);
+}
+
+globals.sort(([, name1], [, name2]) => (name1 > name2 ? 1 : -1));
+
+// Filter out globals which are not constructed with `new`
+// const globalCtors = globals.filter(([type]) => type === 'TypeError');
+const globalCtors = globals.filter(([type]) => !['Symbol', 'BigInt'].includes(type));
+
+const initArgs = {
+	Promise: [() => {}],
+	DataView: [new ArrayBuffer(8)],
+	FinalizationRegistry: [() => {}],
+	WeakRef: [{}]
+};
+
+function createInstance(type) {
+	if (type === 'Function') return function() {};
+	const ctor = global[type];
+	return new ctor(...(initArgs[type] || [])); // eslint-disable-line new-cap
+}
+
+describe('Primitives', () => {
+	it.each([
+		['null', null],
+		['undefined', undefined],
+		['string', 'abc'],
+		['boolean', true],
+		['number', 123],
+		['symbol', Symbol('abc')],
+		// eslint-disable-next-line node/no-unsupported-features/es-builtins
+		...(bigIntSupported ? [['bigint', BigInt(100)]] : [])
+	])('%s', (type, val) => {
+		expect(typeOf(val)).toBe(type);
 	});
 });
+
+describe('Objects', () => {
+	describe.each(globalCtors)('%s', (type) => {
+		it('correctly identified', () => {
+			const instance = createInstance(type);
+			expect(typeOf(instance)).toBe(type);
+		});
+
+		// Impossible to detect Error subclasses where prototype has been altered
+		// https://github.com/overlookmotel/native-type-of/issues/2
+		if (!type.endsWith('Error')) {
+			describe('correctly identified when prototype set to', () => {
+				it('null', () => {
+					const instance = createInstance(type);
+					Object.setPrototypeOf(instance, null);
+					expect(typeOf(instance)).toBe(type);
+				});
+
+				// Impossible to detect Promises where prototype has been altered
+				// https://github.com/overlookmotel/native-type-of/issues/1
+				if (type !== 'Promise') {
+					describe('prototype of', () => {
+						it.each(
+							globals.filter(([protoType]) => (
+								protoType !== type
+								// Objects with prototype `TypeError.prototype` are identified as 'TypeError'
+								// (same applies for other Array subclass prototypes)
+								// https://github.com/overlookmotel/native-type-of/issues/3
+								&& (type !== 'Object' || !isErrorSubclass(protoType))
+							))
+						)('%s', (_, {prototype}) => {
+							const instance = createInstance(type);
+							Object.setPrototypeOf(instance, prototype);
+							expect(typeOf(instance)).toBe(type);
+						});
+					});
+
+					describe('instance of', () => {
+						it.each(
+							globalCtors.filter(([protoType]) => (
+								// Objects with prototype `new TypeError()` are identified as 'TypeError'
+								// (same applies for other Array subclass prototypes)
+								// https://github.com/overlookmotel/native-type-of/issues/3
+								type !== 'Object' || !isErrorSubclass(protoType)
+							))
+						)('%s', (instanceType) => {
+							const instance = createInstance(type);
+							Object.setPrototypeOf(instance, createInstance(instanceType));
+							expect(typeOf(instance)).toBe(type);
+						});
+					});
+				}
+			});
+		}
+	});
+
+	describe('Arguments', () => {
+		it('correctly identified', () => {
+			const instance = (function() { return arguments; }()); // eslint-disable-line prefer-rest-params
+			expect(typeOf(instance)).toBe('Arguments');
+		});
+	});
+});
+
+function isErrorSubclass(type) {
+	return /.Error$/.test(type);
+}
